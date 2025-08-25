607b7c6afe9fd9f6d5ed3a3248cc134c6ce15592
Islomov-Doniyorbek <islomovdoniyorbey@gmail.com>
2025-08-25 16:15:21 +0500

    chatbot

--- bot.js
+++ bot.js
@@ -41,7 +41,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 5000;
 
-server.listen(5000, () => {
+server.listen(PORT, () => {
   console.log("Chatbot server 5000-portda ishlayapti...");
 });
